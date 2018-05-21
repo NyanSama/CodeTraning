93af4c99e8c38c16e25b56a56e986c921e606d36
Nyansama <nyansama@163.com>
2018-05-21 17:03:47 +0800

    add reverse func

--- Leetcode/Python/Linkedlist/Solutions.py
+++ Leetcode/Python/Linkedlist/Solutions.py
@@ -36,6 +36,38 @@ class Solution:
                     cur2 = cur2.next
                     tmp = tmp.next
 
+    def Reverse(self, l):
+        """
+        Reverse Linked list
+        type l: ListNode
+        rtype : ListNode
+        """
+        cur1 = l
+        # if l lenth is 1
+        if cur1.next != None:
+            # set cur2 cur1.next
+            cur2 = cur1.next
+            cur1.next = None
+        else:
+            return cur1
+        
+        while cur2.next != None:
+            # set cur3 for cur2
+            cur3 = cur2.next
+
+            # reverse cur2.next to cur1    cur1 cur2->cur3->..   ---> cur1<-cur2 cur3->..
+            cur2.next = cur1
+            # reset cur1 cur2              []<-cur1  cur2(cur3)->..
+            cur1 = cur2
+            cur2 = cur3
+        
+        # set final link
+        cur2.next = cur1
+
+        return cur2
+
+
+
 
 def mergeTwoLists_test(l1,l2):
     """
@@ -60,6 +92,25 @@ def mergeTwoLists_test(l1,l2):
 
     return ans.ToList()
 
-if __name__ == 'main':
-    ans = mergeTwoLists_test([1,2,3,4],[1,2,3,4,5])
+def Reverse_test(l):
+    """
+    Test for Reverse
+    type l: List
+    rtype: List
+    """
+    ll = LinkList()
+    ll.InitFromList(l)
+    sol = Solution()
+    ans_pt = sol.Reverse(ll.list)
+    ans = LinkList()
+    ans.InitFromPt(ans_pt)
+    return ans.ToList()
+
+
+
+if __name__ == "__main__":
+    #ans = mergeTwoLists_test([1,2,3,4],[1,2,3,4,5])
+    print(Reverse_test([1,2,3,4,5,6]))
+
+   # print("helloWhorld")
     
